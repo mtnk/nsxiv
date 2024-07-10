63a6930a77bf061ce7838aa29e1fc5b5ff7694c5
mtnk <mtt5955@gmail.com>
2024-07-10 14:10:33 +0900

    change thumbnail sizes

--- config.def.h
+++ config.def.h
@@ -74,7 +74,7 @@ static const bool ALPHA_LAYER = false;
 #ifdef INCLUDE_THUMBS_CONFIG
 
 /* thumbnail sizes in pixels (width == height): */
-static const int thumb_sizes[] = { 32, 64, 96, 128, 160 };
+static const int thumb_sizes[] = { 64, 128, 192, 256 };
 
 /* thumbnail size at startup, index into thumb_sizes[]: */
 static const int THUMB_SIZE = 3;
